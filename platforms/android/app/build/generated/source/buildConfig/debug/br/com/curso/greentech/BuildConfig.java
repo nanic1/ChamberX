a1cb7eb417f53f665db7c482231fb3da36cc869e
nanic <pedrokurtz.01@outlook.com>
2024-08-27 17:42:13 -0300

    1.0.0

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/br/com/curso/greentech/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.curso.greentech;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.com.curso.greentech";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "1.0.0";
+}
